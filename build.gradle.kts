e3c2715bb17f9e11bbe70015914c9d2ab1b96e00
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-01 19:18:13 +0000

    Setup | Added Gradle Wrapper / Settings / Build Script

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    repositories {
+        mavenCentral()
+        jcenter()
+        google()
+    }
+
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.4.0"))
+    }
+}
+
+plugins {
+    idea
+    kotlin("jvm") version "1.4.0"
+}
+
+allprojects {
+    group = "io.github.tomplum"
+    version = "1.0.0"
+
+    apply(plugin = "kotlin")
+
+    repositories {
+        mavenCentral()
+        jcenter()
+        google()
+    }
+
+    dependencies {
+        implementation(kotlin("stdlib-jdk8"))
+        implementation(kotlin("reflect"))
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+}
