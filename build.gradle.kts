eee975e23da7daf9475bd3349566e5fb8d6d4d1d
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-02 17:31:35 +0000

    Gradle | Added Maven Publish Plugin + Configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,39 +14,56 @@ buildscript {
 
 plugins {
     idea
+    `maven-publish`
     kotlin("jvm") version "1.4.0"
 }
 
-allprojects {
-    group = "io.github.tomplum"
-    version = "1.0.0"
+group = "io.github.tomplum"
+version = "1.0.0"
 
-    apply(plugin = "kotlin")
+apply(plugin = "kotlin")
 
+repositories {
+    mavenCentral()
+    jcenter()
+    google()
+}
+
+dependencies {
+    //Standard Libraries
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+
+    //Logging
+    implementation("org.slf4j:slf4j-api:1.7.30")
+    runtimeOnly("org.apache.logging.log4j:log4j-core:2.13.0")
+
+    //Testing
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.4.2")
+    testImplementation("org.junit.platform:junit-platform-launcher:1.3.1")
+    testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.20")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+publishing {
     repositories {
-        mavenCentral()
-        jcenter()
-        google()
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/tomplum/advent-of-code-libs")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
     }
-
-    dependencies {
-        //Standard Libraries
-        implementation(kotlin("stdlib-jdk8"))
-        implementation(kotlin("reflect"))
-
-        //Logging
-        implementation("org.slf4j:slf4j-api:1.7.30")
-        runtimeOnly("org.apache.logging.log4j:log4j-core:2.13.0")
-
-        //Testing
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
-        testImplementation("org.junit.jupiter:junit-jupiter-params:5.4.2")
-        testImplementation("org.junit.platform:junit-platform-launcher:1.3.1")
-        testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.20")
-    }
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+        }
     }
 }
