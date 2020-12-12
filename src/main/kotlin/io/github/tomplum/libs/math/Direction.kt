b2078c4d1def71294a41df3448720ee6d012012c
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-12-12 17:40:15 +0000

    Math | Added Direction Kotlin Doc

--- src/main/kotlin/io/github/tomplum/libs/math/Direction.kt
+++ src/main/kotlin/io/github/tomplum/libs/math/Direction.kt
@@ -1,5 +1,9 @@
 package io.github.tomplum.libs.math
 
+/**
+ * The relative direction at given angle from the observer.
+ * @param degree The degree of the angle. A multiple of 45 between 0 and 360.
+ */
 enum class Direction(private val degree: Int) {
     UP(0),
     TOP_RIGHT(45),
@@ -10,6 +14,12 @@ enum class Direction(private val degree: Int) {
     LEFT(270),
     TOP_LEFT(315);
 
+    /**
+     * Rotates the current direction by the given [angle].
+     * @param angle A multiple of 45 between 0 and 360. Can be positive or negative.
+     * @throws IllegalArgumentException if the [angle] is invalid.
+     * @return The new facing direction after rotating by the given [angle].
+     */
     fun rotate(angle: Int): Direction = values()
         .find { it.degree == normalise(angle) }
         ?: throw IllegalArgumentException("Invalid Angle $angle")
