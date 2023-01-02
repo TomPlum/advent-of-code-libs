0a6e2535a98f9db7713b76f4f74aaa25f4040114
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2023-01-02 12:21:56 +0000

    Testing | Fixed failing BenchmarkReaderTest

--- library/src/test/kotlin/io/github/tomplum/libs/solutions/benchmark/utility/BenchmarkReaderTest.kt
+++ library/src/test/kotlin/io/github/tomplum/libs/solutions/benchmark/utility/BenchmarkReaderTest.kt
@@ -14,10 +14,10 @@ class BenchmarkReaderTest {
         val fileLocation = "${System.getProperty("user.dir")}/src/test/resources/benchmark.xml"
         val result = benchmarkReader.read(fileLocation)
         assertThat(result!!.results).containsExactly(
-            Benchmark(1, 67633, 199628, 35252946, 2153846),
-            Benchmark(2, 11449, 13187, 2910280, 2595123),
-            Benchmark(3, 7581, 2525, 4133315, 6328752),
-            Benchmark(4, 413, 806, 11498876, 10974261)
+            Benchmark(1, "67633", "199628", 35252946, 2153846),
+            Benchmark(2, "11449", "13187", 2910280, 2595123),
+            Benchmark(3, "7581", "2525", 4133315, 6328752),
+            Benchmark(4, "413", "806", 11498876, 10974261)
         )
     }
 }
