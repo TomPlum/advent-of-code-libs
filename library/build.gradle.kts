2e8afed02c14aed2533395a93c70945589f43987
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2023-01-05 08:44:23 +0000

    Math | AdventMap2D - Added Initialise Function

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -6,6 +6,7 @@ version = releaseVersion.toString()
 dependencies {
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.14.0")
 
+    testImplementation(project(":advent-of-code-test-support"))
     testImplementation("io.mockk:mockk:1.13.3")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
 }
--- library/src/main/kotlin/io/github/tomplum/libs/math/map/AdventMap2D.kt
+++ library/src/main/kotlin/io/github/tomplum/libs/math/map/AdventMap2D.kt
@@ -14,6 +14,29 @@ import io.github.tomplum.libs.math.point.Point2D
 abstract class AdventMap2D<T: MapTile<*>>: AdventMap<Point2D, T>() {
 
     /**
+     * Initialises the map based on the given [data].
+     * The [data] parameter should be a list of [String]
+     * where each character represents a value in the grid.
+     *
+     * @param data A collection of row data of the map
+     * @param tileConstructor Constructs a new map tile with the given value
+     */
+    protected fun init(data: Collection<String>, tileConstructor: (value: Any) -> T) {
+        var x = 0
+        var y = 0
+        data.forEach { row ->
+            row.forEach { column ->
+                val tile = tileConstructor(column)
+                val position = Point2D(x, y)
+                addTile(position, tile)
+                x++
+            }
+            x = 0
+            y++
+        }
+    }
+
+    /**
      * Gets all the tiles that are orthogonally adjacent to the given [positions].
      * @param positions The set of positions whose adjacent tiles will be found.
      * @return a [Map] of orthogonally-adjacent [Point2D] and their respective tiles [T].
